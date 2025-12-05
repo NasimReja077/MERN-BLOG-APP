dfc915912541aac2cb192ec680fcf5111026c222
Nasim <nasim.reja.077@gmail.com>
2025-12-05 23:53:26 +0530

    Add router

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,47 @@
-import {TestButton} from "./Test/TestButton"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 const App =()=> {
-  return (
-    <div>
-    <TestButton/>
-    </div>
-  )
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Applayout/>,
+      errorElement: <ErrorPage/>,
+      children: [
+        {
+          path: "/",
+          element: <Home/>
+        },
+        {
+          path: "/login",
+          element: <Login/>
+        },
+        {
+          path: "/register",
+          element: <Register/>
+        },
+        {
+          path: "/blog/:id",
+          element: <BlogDetail/>
+          loader: 
+        },
+        {
+          path: "/profile",
+          element: <Profile/>
+          loader: 
+        },
+        {
+          path: "/create-blog",
+          element: <CreateBlog/>
+          action:
+        },
+
+        { // 2nd mathod for error page
+          path: "*",
+          element: <NotFound />
+        }
+      ]
+    }
+  ])
+  return <RouterProvider router={router}/>
 }
 
 export default App
