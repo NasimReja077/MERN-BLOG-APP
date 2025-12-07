2335b80e767ae90e7d76a68920662ba90d457d28
Nasim <nasim.reja.077@gmail.com>
2025-12-07 22:57:40 +0530

    chore(store): add comment reducer and disable serializableCheck in middleware
    - Added commentReducer to Redux store for managing comment state
    - Enabled custom middleware configuration using getDefaultMiddleware
    - Disabled serializableCheck to prevent warnings for non-serializable values (FormData, File, toast, etc.)
    - Improved store structure for better scalability and feature separation

--- Frontend/src/store/store.js
+++ Frontend/src/store/store.js
@@ -1,7 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authReducer from './features/authSlice';
+import blogReducer from './features/blogSlice';
+import commentReducer from './features/commentSlice';
+
 export const store = configureStore({
      reducer: {
           auth: authReducer,
-     }
-})
+          blog: blogReducer,
+          comment: commentReducer,
+     },
+     middleware: (getDefaultMiddleware) => 
+          getDefaultMiddleware({
+               serializableCheck: false,
+          }),
+});
+
+
+
+// https://redux-toolkit.js.org/api/getDefaultMiddleware
+// https://redux-toolkit.js.org/api/serializabilityMiddleware
+// https://sunnychopper.medium.com/how-to-use-redux-middleware-to-better-control-your-data-and-write-cleaner-code-33f129d1cad0
