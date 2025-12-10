039be76395a4c816197c202d3baec8cbfd3deebf
Nasim <nasim.reja.077@gmail.com>
2025-12-10 23:41:44 +0530

    Add Axios response

--- Frontend/src/api/axios.js
+++ Frontend/src/api/axios.js
@@ -4,36 +4,55 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3000
 
 const axiosInstance = axios.create({
      baseURL: API_BASE_URL,
-     withCredentials: true, // important for cookies
+     withCredentials: true, // Sends HTTP-only cookies automatically
      timeout: 10000, // 10 seconds
-     headers: {
-          'Content-Type' : 'application/json',
-     },
+     // headers: {
+     //      'Content-Type' : 'application/json',
+     // },
 });
 
 // Request interceptor
-axiosInstance.interceptors.request.use( // use accepts two functions: the onFulfilled (runs before request) and the onRejected (runs if there’s an error creating the request).
-     (config) => {
-          const token = localStorage.getItem('token');
-          if(token){
-               config.headers.Authorization = `Bearer ${token}`;
-          }
-          return config;
-     },
-     (error) => Promise.reject(error)
-);
+// axiosInstance.interceptors.request.use( // use accepts two functions: the onFulfilled (runs before request) and the onRejected (runs if there’s an error creating the request).
+//      (config) => {
+//           // const token = localStorage.getItem('token');
+//           // if(token){
+//           //      config.headers.Authorization = `Bearer ${token}`;
+//           // }
+//           return config;
+//      },
+//      (error) => Promise.reject(error)
+// );
 
 // Response interceptor
 
+// axiosInstance.interceptors.response.use(
+//      (response) => response,
+//      (error) => {
+//           if (error.response?.status === 401){
+//                // localStorage.removeItem('token');
+//                // window.location.href = '/login';
+//                // Let React handle logout & redirect
+//                // console.warn("Unauthorized – redirect handled by auth flow");
+//                // Cookie expired or invalid - redirect to login
+//                window.location.href = '/login';
+//           }
+//           return Promise.reject(error);
+//      }
+// );
+
+
 axiosInstance.interceptors.response.use(
-     (response) => response,
-     (error) => {
-          if (error.response?.status === 401){
-               localStorage.removeItem('token');
-               window.location.href = '/login';
-          }
-          return Promise.reject(error);
-     }
-);
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      window.location.href = '/login';
+    }
+    if (error.response?.status === 429) {
+      console.warn("Rate limited:", error.response.data);
+    }
+    return Promise.reject(error);
+  }
+)
+
 
 export default axiosInstance;
