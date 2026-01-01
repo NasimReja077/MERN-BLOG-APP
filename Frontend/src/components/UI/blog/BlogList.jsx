4b6f9d92b20c6b1d8e55f5323140a9e0bbfc61b3
Nasim <nasim.reja.077@gmail.com>
2026-01-01 19:51:09 +0530

    add PropTypes, and format

--- Frontend/src/components/UI/blog/BlogList.jsx
+++ Frontend/src/components/UI/blog/BlogList.jsx
@@ -1,9 +1,12 @@
+// Frontend/src/components/UI/blog/BlogList.jsx
 import React from 'react';
+import PropTypes from 'prop-types';
 import { BlogCard } from './BlogCard';
 import { Loading } from '../../feedback/Loading';
+import { formatCompactNumber } from '../../../utils/formatters';
 
 
-export const BlogList = ({ blogs, loading }) => {
+export const BlogList = ({ blogs = [], loading = false }) => {
   if (loading) {
     return <Loading />;
   }
@@ -17,10 +20,25 @@ export const BlogList = ({ blogs, loading }) => {
   }
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-      {blogs.map((blog) => (
-        <BlogCard key={blog._id} blog={blog} />
-      ))}
-    </div>
+    <>
+      <div className="mb-6 text-sm text-gray-500">Showing {formatCompactNumber(blogs.length)} stories</div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        {blogs.map((blog) => (
+          <BlogCard key={blog._id} blog={blog} />
+        ))}
+      </div>
+    </>
   );
+};
+
+BlogList.propTypes = {
+  blogs: PropTypes.arrayOf(
+    PropTypes.shape({
+      _id: PropTypes.string.isRequired,
+      title: PropTypes.string,
+      summary: PropTypes.string,
+      thumbnail: PropTypes.string,
+    })
+  ),
+  loading: PropTypes.bool,
 };
